e1f099f9d8cb1baf6bcc2929c84d0be0117a67b5
José Acerbi <joseacerbialmeidaneto@hotmail.com>
2025-06-05 09:29:22 -0300

    feat: realiza conexao com banco de dados

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ZettaLab-Desafio2
 Projeto desenvolvido como solução para o desafio 2 da Zetta Lab 2025
 
-TypeScrip, NodeJS, Express, MySQL
+TypeScrip, NodeJS, Express, MySQL, sequelize
+
+deve se criar manualmente um banco no mysql
 
 npm run dev para executar normalmente
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import express from 'express';
+import sequelize from './config/database';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send("Server is running!")
+  res.status(200).send("Server is running!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connected to database!');
+    return sequelize.sync();
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to database:', error);
+  });
